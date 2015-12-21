4acccb30f5adbd83cd4d37507a9ed59ca1a4bbb4
Dominic Muttel <dominic.muttel@gmail.com>
2015-12-21 12:30:58 -0700

    image gallery added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
-// add scripts
 
-$(document).on('ready', function() {
+$(document).on('ready', function(e){
 
+  // $('#web-projects','#art-projects','#design-projects').bxSlider({
+  //   mode: 'horizontal',
+  //   useCSS: true,
+  //   minSlides: 3,
+  //   maxSlides: 3,
+  //   slideWidth: 360,
+  //   slideMargin: 10
+  // });
 
-  // var clientHeight = $( window ).height();
-  // $('.background').css('height', clientHeight);
+  var slider = $('.page-titles').bxSlider({
+    mode: 'horizontal',
+    pager: false,
+  });
 
+  $('#web').click(function(e){
+    slider.goToSlide(1);
+    e.preventDefault();
+    return false;
+  });
+  $('#design').click(function(e){
+    slider.goToSlide(3);
+    e.preventDefault();
+    return false;
+  });
+  $('#art').click(function(e){
+    slider.goToSlide(2);
+    e.preventDefault();
+    return false;
+  });
+  $('#name').click(function(e){
+    slider.goToSlide(0);
+    e.preventDefault();
+    return false;
+  });
 
+  e.preventDefault();
 });
+
+
