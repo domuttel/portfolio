18c514b464bbd4057a4d96427606d8fdb06fef8a
Dominic Muttel <dominic.muttel@gmail.com>
2015-12-23 13:30:07 -0700

    added all features roughly

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 
 $(document).on('ready', function(){
   $('#web-projects').bxSlider({
-    slideWidth: 300,
-    minSlides: 2,
-    maxSlides: 2,
-    slideMargin: 100
+    slideWidth: 200,
+    minSlides: 3,
+    maxSlides: 3,
+    slideMargin: 10
+  });
+
+  $("#demo01").animatedModal({
+    modalTarget:'modal-02'
+  });
 });
 
-});
 
   var slider = $('.page-titles').bxSlider({
     mode: 'horizontal',
