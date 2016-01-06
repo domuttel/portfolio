77e40b4b6e786ba319edad8785fd84b172f2bde7
Dominic Muttel <dominic.muttel@gmail.com>
2016-01-06 14:14:39 -0700

    massive changes on large screens

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 
 $(document).on('ready', function(){
   $('.projects').bxSlider({
-    slideWidth: 200,
+    slideWidth: 250,
+    // slideHeight: 5,
+    controls: false,
     minSlides: 3,
     maxSlides: 3,
-    slideMargin: 10
+    slideMargin: 50,
+    // adaptiveHeight: false
+    // responsive: true
   });
 
+$( "#about-me > h4:nth-child(1)" ).css( "color:black;" );
 
 });
+
 
 
   var slider = $('.page-titles').bxSlider({
     mode: 'horizontal',
     pager: false,
     controls: false,
-    useCSS: true,
+    moveSlides: 3
+    // useCSS: true,
+    // responsive: true,
   });
-
   $('#web').click(function(){
     slider.goToSlide(1);
     return false;
@@ -34,3 +41,6 @@ $(document).on('ready', function(){
     slider.goToSlide(0);
     return false;
   });
+
+
+
